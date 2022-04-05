87ff7f34fb1b2f4079c000fabd58414a30e0de5e
CreatorPithadiya <100771124+CreatorPithadiya@users.noreply.github.com>
2022-04-05 12:39:13 +0530

    Update Crud.js

--- src/Component/Crud.js
+++ src/Component/Crud.js
@@ -7,7 +7,8 @@ export default function Crud() {
     return(
             <Layout>
                 <Content>
-                    register userss
+                    <h1>CRUD-with-Redux</h1>
+                    <img src="https://s33046.pcdn.co/wp-content/uploads/2018/12/word-image-228.png" alt="Sperious" style={{marginTop: 20, width: '100%'}}/>
                 </Content>
             </Layout>
     )
